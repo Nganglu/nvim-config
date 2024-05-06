3280eddf2511aedd5b81cc269688db30986b4de5
Benjamin Q.T. Nguyen <Ben.QT.Nguyen@bqtn.com>
2024-05-06 19:35:00 -0400

    Setup rust,floaterm,undotree

--- lua/bqtn/keymaps.lua
+++ lua/bqtn/keymaps.lua
@@ -7,7 +7,9 @@ local map = vim.keymap.set
 map("n","<leader>pv", vim.cmd.Ex,{desc="Show PWD"})
 map("n","<leader>pV", vim.cmd.Sex,{desc="Show Split PWD"})
 
+--Terminal
+map("n","<leader>t", vim.cmd.FloatermToggle,{desc="Terminal"})
 
 --Package Manager
-map("n","<leader>L", vim.cmd.Mason,{desc="Lazy"})
+map("n","<leader>L", vim.cmd.Lazy,{desc="Lazy"})
 map("n","<leader>M", vim.cmd.Mason,{desc="Mason"})
--- lua/bqtn/plugins/lsp-config.lua
+++ lua/bqtn/plugins/lsp-config.lua
@@ -12,7 +12,7 @@ return {
         ensure_installed = {
           "lua_ls",
           "tsserver",
-          "rust_analyzer",
+          --"rust_analyzer", **Configured by Rustacean**
           "jedi_language_server",   --Python
           "jdtls",                  --Java
         },
@@ -25,13 +25,13 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
-      require("plugins.config.rust")
+      --lspconfig.rust_analyzer.setup({})
       lspconfig.jedi_language_server.setup({})
       lspconfig.jdtls.setup({})
 
       vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {desc="Hover (info)"})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {desc="Go to Definition"})
-      vim.keymap.set({ "n", "v" }, "<leader>c", vim.lsp.buf.code_action, {desc="~Code Action~"})
+      vim.keymap.set({ "n", "v" }, "<leader>c", vim.lsp.buf.code_action, {desc="Code Action"})
     end,
   },
 }
