0497313ac22c633c64961e4b55a32b5dc6a9032a
Benjamin Q.T. Nguyen <Ben.QT.Nguyen@bqtn.com>
2024-05-14 14:00:26 -0400

    Configured terminal use + added Deno lsp

--- lua/bqtn/keymaps.lua
+++ lua/bqtn/keymaps.lua
@@ -6,10 +6,15 @@ local map = vim.keymap.set
 --Files
 map("n", "<leader>pv", vim.cmd.Ex, { desc = "Show PWD" })
 map("n", "<leader>pV", vim.cmd.Sex, { desc = "Show Split PWD" })
+map("n", "<leader>w", ":update<CR>", { desc = "Save file"})
 
 --Terminal
-map("n", "<leader>t", vim.cmd.FloatermToggle, { desc = "Terminal" })
-map("n", "<C-t>", vim.cmd.FloatermToggle, { desc = "Terminal" })
+map("n", "<leader>t", vim.cmd.FloatermToggle, { desc = "Toggle Floaterm" })
+map("t", "<C-t>", vim.cmd.FloatermToggle, { desc = "Toggle Floaterm" })
+map("t", "<C-n>", vim.cmd.FloatermNew, { desc = "New Terminal Instance" })
+map("t", "<C-h>", vim.cmd.FloatermPrev, { desc = "Prev Terminal" })
+map("t", "<C-s>", vim.cmd.FloatermNext, { desc = "Next Terminal" })
+map("t", "<C-x>", vim.cmd.FloatermKill, { desc = "Kill Terminal Instance" })
 
 --Package Manager
 map("n", "<leader>L", vim.cmd.Lazy, { desc = "Lazy" })
@@ -28,4 +33,4 @@ function _G.toggle_number_mode()
   end
 end
 
-vim.api.nvim_set_keymap("n", "<leader>r", ":lua toggle_number_mode()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>r", ":lua toggle_number_mode()<CR>", { noremap = true, silent = true, desc = "Toggle relative numbers" } )
--- lua/bqtn/plugins/lsp-config.lua
+++ lua/bqtn/plugins/lsp-config.lua
@@ -11,10 +11,11 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
+          "denols",
           "tsserver",
           --"rust_analyzer", **Configured by Rustacean**
-          "jedi_language_server",   --Python
-          "jdtls",                  --Java
+          "jedi_language_server", --Python
+          "jdtls",           --Java
         },
       })
     end,
@@ -24,14 +25,24 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+      local nvim_lsp = require("lspconfig")
+      nvim_lsp.denols.setup({
+        on_attach = on_attach,
+        root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+      })
+
+      nvim_lsp.tsserver.setup({
+        on_attach = on_attach,
+        root_dir = nvim_lsp.util.root_pattern("package.json"),
+        single_file_support = false,
+      })
       --lspconfig.rust_analyzer.setup({})
       lspconfig.jedi_language_server.setup({})
       lspconfig.jdtls.setup({})
 
-      vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {desc="Hover (info)"})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {desc="Go to Definition"})
-      vim.keymap.set({ "n", "v" }, "<leader>c", vim.lsp.buf.code_action, {desc="Code Action"})
+      vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, { desc = "Hover (info)" })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+      vim.keymap.set({ "n", "v" }, "<leader>c", vim.lsp.buf.code_action, { desc = "Code Action" })
     end,
   },
 }
